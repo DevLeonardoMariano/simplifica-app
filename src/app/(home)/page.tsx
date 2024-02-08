0b6a03cb1799a74e41f07d44998c921ca066555a
leonardo.santos <leonardo.mariano@bevioficial.com.br>
2024-02-08 15:55:48 -0300

    starting project with dashboard

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { Card } from '../_components/ui/card';
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Card className="w-full h-[50px] bg-lime-600 text-slate-100 rounded-none flex flex-col justify-center">
+          <p>Leonardo Mariano</p>
+        </Card>
+      </div>
+    </>
+  );
+}
